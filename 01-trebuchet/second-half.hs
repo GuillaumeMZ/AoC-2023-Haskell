ed2b317eb25581070ecbf6b541f3d82e35f7b2f8
Guillaume M <58004135+GuillaumeMZ@users.noreply.github.com>
2023-12-01 16:50:50 +0100

    solve second half of day 01

---
+++ 01-trebuchet/second-half.hs
@@ -0,0 +1,24 @@
+import Data.Char (isDigit)
+
+calibration :: String -> Int
+calibration source =  
+    read [head digits, last digits] 
+        where digits = filter isDigit source
+
+digitify :: String -> String
+digitify ('o':'n':'e':tl)         = "1" ++ digitify ("e" ++ tl)
+digitify ('t':'w':'o':tl)         = "2" ++ digitify ("o" ++ tl)
+digitify ('t':'h':'r':'e':'e':tl) = "3" ++ digitify ("e" ++ tl)
+digitify ('f':'o':'u':'r':tl)     = "4" ++ digitify ("r" ++ tl)
+digitify ('f':'i':'v':'e':tl)     = "5" ++ digitify ("e" ++ tl)
+digitify ('s':'i':'x':tl)         = "6" ++ digitify ("x" ++ tl)
+digitify ('s':'e':'v':'e':'n':tl) = "7" ++ digitify ("n" ++ tl)
+digitify ('e':'i':'g':'h':'t':tl) = "8" ++ digitify ("t" ++ tl)
+digitify ('n':'i':'n':'e':tl)     = "9" ++ digitify ("e" ++ tl)
+digitify (hd:tl)                  = hd : digitify tl
+digitify []                       = ""
+
+main :: IO ()
+main = do
+    input <- getContents
+    print $ sum . map (calibration . digitify) . words $ input
