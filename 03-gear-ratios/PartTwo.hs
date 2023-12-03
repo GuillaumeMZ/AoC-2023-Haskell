59248b4745ec91e6a872049e3568c0a93fcbe573
Guillaume M <58004135+GuillaumeMZ@users.noreply.github.com>
2023-12-03 16:07:21 +0100

    solve second half of day 03

--- 03-gear-ratios/PartTwo.hs
+++ 03-gear-ratios/PartTwo.hs
@@ -1,5 +1,24 @@
+import qualified Data.Text as T
+import qualified Data.Text.IO as T.IO
+
 import Common
+
+associatedNumbers :: [Number] -> Symbol -> [Number]
+associatedNumbers numbers symbol = filter ((flip isNumberNearSymbol) symbol) numbers
+
+tupleize :: [a] -> (a, a)
+tupleize [v1, v2] = (v1, v2)
+
+mult :: (Number, Number) -> Int
+mult (a, b) = value a * value b
 
 main :: IO ()
 main = do
-    print 1
+    input <- T.IO.getContents
+
+    let (numbers, symbols) = parseNumbersAndSymbols (T.lines input) 1
+    print $ sum .
+            map (mult . tupleize) .
+            filter ((==2) . length) .
+            map (associatedNumbers numbers) . 
+            filter ((=='*') . symbol) $ symbols
