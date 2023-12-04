84754e040fb35247405e83f9c20f18ae4f35ec0a
GuillaumeMZ <58004135+GuillaumeMZ@users.noreply.github.com>
2023-12-04 14:46:21 +0100

    solve second half of day 04

--- 04-scratchcards/PartTwo.hs
+++ 04-scratchcards/PartTwo.hs
@@ -1,5 +1,22 @@
+import qualified Data.Text as T
+import qualified Data.Text.IO as T.IO
+
 import Common
+
+mapFirstN :: Int -> (a -> a) -> [a] -> [a]
+mapFirstN n f l = map f (take n l) ++ drop n l
+
+-- assuming len 1 == len 2
+scratchcardTotal :: [Int] -> [Int] -> Int
+scratchcardTotal [] [] = 0
+scratchcardTotal (winCount:winRest) (cardCount:cardRest) = 
+    cardCount + scratchcardTotal winRest computedCardRest where
+        computedCardRest = mapFirstN winCount (cardCount+) cardRest
 
 main :: IO ()
 main = do
-    undefined
+    input <- T.IO.getContents
+    let cards = parseCards input
+        possessedWinningNumbersCount = map (length . possessedWinningNumbers) cards
+
+    print $ scratchcardTotal possessedWinningNumbersCount (take (length possessedWinningNumbersCount) $ iterate (*1) 1)
